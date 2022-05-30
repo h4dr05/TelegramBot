88771d44d56feaae1792bfac2b5920c7a6bccdff
Amaan Q <amaanq12@gmail.com>
2022-05-30 02:10:34 -0400

    cleaning up, use a .token file w/ token for security measures

--- bot.py
+++ bot.py
@@ -1,20 +1,22 @@
-#Import components
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
+import telegram.ext
+
 
 def sms(bot, update):
     print('Somebody sent me a /start command, what should I do?')
-    bot.message.reply_text('Hello, I am a useless bot (right now at least) \nI am learning though...')
+    bot.message.reply_text(
+        'Hello, I am a useless bot (right now at least) \nI am learning though...')
 
 
-#main function
-def main():
-    my_bot = Updater("5313059421:AAEHlVfKWG-DiPS5krh3LToqR_YlNkytfdo", "https://telegg.ru/orig/bot", use_context=True)
+# Initiate the bot
+def run():
+    token_fd = open('.token', 'r')
+    token = token_fd.read().strip()
+    token_fd.close()
 
-    my_bot.dispatcher.add_handler(CommandHandler('start', sms))
-    
-    my_bot.start_polling() # checking for messages from Telegram
-    my_bot.idle() #bot is working until it's stopped
+    my_bot = telegram.ext.Updater(token=token,
+                                  base_url="https://telegg.ru/orig/bot",
+                                  use_context=True)
 
-
-main()
+    my_bot.dispatcher.add_handler(telegram.ext.CommandHandler('start', sms))
+    my_bot.start_polling()  # checking for messages from Telegram
+    my_bot.idle()  # bot is working until it's stopped
