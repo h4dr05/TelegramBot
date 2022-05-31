bf5a06ca67f364d8de80545a4bd8b3655a6436ba
Hadros <fungigame@gmail.com>
2022-05-31 02:05:47 +0200

    fixed url issue and fixed Bot.sms

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telegram.ext
 
 
 class Bot(telegram.ext.Updater):
-    def __init__(self, token, base_url="https://telegg.ru/orig/bot", use_context=True):
+    def __init__(self, token, base_url="https://api.telegram.org/bot", use_context=True):
         super(Bot, self).__init__(token=token,
                                   base_url=base_url, use_context=use_context)
 
@@ -12,7 +12,7 @@ class Bot(telegram.ext.Updater):
         self.start_polling()  # checking for messages from Telegram
         self.idle()  # bot is working until it's stopped
 
-    def sms(bot, update):
+    def sms(self, bot, update):
         print('Somebody sent me a /start command, what should I do?')
         bot.message.reply_text(
             'Hello, I am a useless bot (right now at least)\nI am learning though...')
