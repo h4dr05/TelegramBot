06a24dda17eb1aa0a712d86952961e4ca62ed564
Hadros <fungigame@gmail.com>
2022-06-17 06:36:53 +0200

    added notes, fixed bug with bot spamming

--- anotherbot.py
+++ anotherbot.py
@@ -1,102 +1,114 @@
-import telebot
-from telebot import types
+import ast
+import json
 
+import telebot
+from bson import json_util
+from telebot import types
 from database import BooksDatabase
+
 
 bot = telebot.TeleBot("5313059421:AAEHlVfKWG-DiPS5krh3LToqR_YlNkytfdo")
 database = BooksDatabase()
 
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    keyboard = types.InlineKeyboardMarkup()
-    keyboard.row(types.InlineKeyboardButton('Search', callback_data='Search'))
-    mesg = bot.send_message(message.chat.id, 'Click Search to find the book you want',
-                            reply_markup=keyboard)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    keyboard.row('Search')
+    mesg = bot.send_message(message.chat.id,'Click search, to... search', reply_markup=keyboard)
     bot.register_next_step_handler(mesg, mainmenu)
 
-
-@bot.callback_query_handler(func=lambda call: True)
-def handler(call: types.CallbackQuery):
-    keyboard = types.InlineKeyboardMarkup()
-    keyboard.row(types.InlineKeyboardButton('Cancel', callback_data='Cancel'))
-    match call.data:
-        case 'Search':
-            mainmenu(call.message)
+def findBook(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    keyboard.row('Cancel')
+    match message.text:
         case 'By title':
-            msg = bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text='Type book name', reply_markup=keyboard)
-            # searchbytitle(call.message, call.data)
+            msg = bot.send_message(message.chat.id, 'Введите название книги', reply_markup=keyboard)
             bot.register_next_step_handler(msg, searchbytitle)
             return
         case 'By description':
-            msg = bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text='Type book desc', reply_markup=keyboard)
+            msg = bot.send_message(message.chat.id, 'Введите описание книги', reply_markup=keyboard)
             bot.register_next_step_handler(msg, searchbydescription)
             return
+
         case 'By author':
-            msg = bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text='Type book author', reply_markup=keyboard)
+            msg = bot.send_message(message.chat.id, 'Введите автора книги', reply_markup=keyboard)
             bot.register_next_step_handler(msg, searchbyauthor)
             return
-        case 'Cancel':
-            mainmenu(call.message)
         case _:
-            msg = bot.send_message(call.message.chat.id,
-                                   'Something went wrong eh')
+            msg = bot.send_message(message.chat.id, 'Something went wrong')
 
 
 def mainmenu(message):
-    keyboard = types.InlineKeyboardMarkup()
-    keyboard.row_width = 3
-    keyboard.row(types.InlineKeyboardButton('By title', callback_data='By title'),
-                 types.InlineKeyboardButton('By description', callback_data='By description'),
-                 types.InlineKeyboardButton('By author', callback_data='By author'))
-    msg = bot.edit_message_text(chat_id=message.chat.id, message_id=message.id, text='Choose the search method', reply_markup=keyboard)
+    print(message.text)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    keyboard.row('By title', 'By description', 'By author')
+    msg = bot.send_message(message.chat.id, 'Choose the search method', reply_markup=keyboard)
+    bot.register_next_step_handler(msg, findBook)
 
-
-# def findBook(call: types.CallbackQuery):
-#     keyboard = types.ReplyKeyboardMarkup(True, False)
-#     keyboard.row('Cancel')
-#     match call.message:
-#         case 'By title':
-#             msg = bot.send_message(call.message.chat.id, 'Type book name', reply_markup=keyboard)
-#             bot.register_next_step_handler(msg, searchbytitle)
-#             return
-#         case 'By description':
-#             msg = bot.send_message(call.message.chat.id, 'Type book desc', reply_markup=keyboard)
-#             bot.register_next_step_handler(msg, searchbydescription)
-#             return
-#
-#         case 'By author':
-#             msg = bot.send_message(call.message.chat.id, 'Type book author', reply_markup=keyboard)
-#             bot.register_next_step_handler(msg, searchbyauthor)
-#             return
-#         case _:
-#             msg = bot.send_message(call.message.chat.id,
-#                                    'Что-то пошло не так, иди нахуй, на кнопки понажимай, сука ебаная')
-
+# @bot.message_handler()
 def searchbytitle(message):
-    found = database.find_book(message.text, database.TITLE)
-    text = database.to_string(list(found))
-    msg = bot.send_message(message.chat.id, text)
-    bot.register_next_step_handler(msg, mainmenu)
-    msg = None
+    msg = ''
+    found = list(database.find_book(message.text, database.TITLE))
+    keyboard = types.InlineKeyboardMarkup()
+    i = 0
+    keyboard.row(types.InlineKeyboardButton('Note', callback_data=f"Note: {json_util.dumps(found[i]['_id'])}"))
+    if message.text == 'Cancel':
+        mainmenu(message)
+    else:
+        while i < len(found):
+            bot.send_photo(message.chat.id, found[i]['thumbnailUrl'])
+            msg = bot.send_message(message.chat.id, found[i]['title'], reply_markup=keyboard)
+            i = i + 1
+        bot.register_next_step_handler(msg, searchbytitle)
 
 
 # @bot.message_handler()
 def searchbydescription(message):
-    found = database.find_book(message.text, database.DESCRIPTION)
-    text = database.to_string(list(found))
-    msg = bot.send_message(message.chat.id, text)
-    bot.register_next_step_handler(msg, mainmenu)
-    msg = None
-
+    msg = ''
+    found = list(database.find_book(message.text, database.DESCRIPTION))
+    keyboard = types.InlineKeyboardMarkup()
+    i = 0
+    keyboard.row(types.InlineKeyboardButton('Note', callback_data=f"Note: {json_util.dumps(found[i]['_id'])}"))
+    if message.text == 'Cancel':
+        mainmenu(message)
+    else:
+        while i < len(found):
+            bot.send_photo(message.chat.id, found[i]['thumbnailUrl'])
+            msg = bot.send_message(message.chat.id, found[i]['title'], reply_markup=keyboard)
+            i = i + 1
+        bot.register_next_step_handler(msg, searchbydescription)
 
 # @bot.message_handler()
 def searchbyauthor(message):
-    found = database.find_book(message.text, database.AUTHOR)
-    text = database.to_string(list(found))
-    msg = bot.send_message(message.chat.id, text)
-    bot.register_next_step_handler(msg, mainmenu)
-    msg = None
+    msg = ''
+    found = list(database.find_book(message.text, database.AUTHOR))
+    keyboard = types.InlineKeyboardMarkup()
+    i = 0
+    keyboard.row(types.InlineKeyboardButton('Note', callback_data=f"Note: {json_util.dumps(found[i]['_id'])}"))
+    if message.text == 'Cancel':
+        mainmenu(message)
+    else:
+        while i < len(found):
+            bot.send_photo(message.chat.id, found[i]['thumbnailUrl'])
+            msg = bot.send_message(message.chat.id, found[i]['title'], reply_markup=keyboard)
+            i = i + 1
+        bot.register_next_step_handler(msg, searchbyauthor)
+
+@bot.callback_query_handler(func=lambda call: True)
+def handler(call: types.CallbackQuery):
+    addNote(call.message, call.data)
 
 
+def addNote(message, book):
+    msg = bot.send_message(message.chat.id,
+                            'Insert the Note' )
+
+    bot.register_next_step_handler(msg, note, book[19: len(book)-1])
+
+
+def note(message, book_id):
+    print(book_id)
+    database.addNote(book_id, message.text)
 bot.infinity_polling()
